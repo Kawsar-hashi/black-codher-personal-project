411463e91a8b12ad8fbaaf6ea16dbd7e9de2852d
Kawsar <kawsar@macbooks-air.home>
2021-01-11 19:11:25 +0000

    cards updated

--- client/src/components/homepage/About.js
+++ client/src/components/homepage/About.js
@@ -5,7 +5,9 @@ class About extends Component {
         return (
             <div classname= "container">
                 <p>
-About this app.
+Welcome to Setle. An app designed by Kawsar Hashi as part of the Black Codher Bootcamp Mentorship programme. This app is designed to showcase my front and backend capabilities where i demonstrate my MERN knowledge. This is my first Full stack app project so its not extremely complicated but is a cool way to showcase my skills. This app is a games and quiz app where users can sign in and play a game and save their progress.
+ Thank you for checking this out. 
+      Kawsar x
                 </p>
             </div>
         );
--- client/src/components/homepage/Hero.js
+++ client/src/components/homepage/Hero.js
@@ -8,7 +8,7 @@ function Hero() {
         <div className='hero-container'>
             <video src="/Videos/video-1.mp4" autoPlay loop muted />
             <h1>ARE YOU READY?</h1>
-           
+            <p>SETLE IN</p>   
             <div className="hero-btns">
                 <Button 
                 className='btns' 
